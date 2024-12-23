0f94691c4f4786f612218d53f832b46bdf73e6a5
Matauskas <matasp2002@gmail.com>
2024-12-23 13:35:20 +0200

    image upload

--- Serverland/obj/Debug/net8.0/Serverland.AssemblyInfo.cs
+++ Serverland/obj/Debug/net8.0/Serverland.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Serverland")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e08d7206f97f993325e10489adc48c9232466f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e204ac94447df437468d22a7a4cbed2087df1a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Serverland")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Serverland")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
